d0fbf804e9db2d8a928bc995d4abff984ff02421
willymx <132931982+willymx@users.noreply.github.com>
2025-06-25 04:09:37 -0600

    Delete temp/larkon directory

--- temp/larkon/Larkon/app/Views/partials/vendor-scripts.php
+++
@@ -1,5 +0,0 @@
-<!-- Vendor Javascript (Require in all Page) -->
-<script src="/js/vendor.js"></script>
-
-<!-- App Javascript (Require in all Page) -->
-<script src="/js/app.js"></script>
--- temp/larkon/Larkon/public/source/js/components/form-wizard.js
+++
@@ -1,9 +0,0 @@
-/*
-Template Name: Larkon - Responsive 5 Admin Dashboard
-Author: Techzaa
-File: wizard Js File
-*/
-
-new Wizard('#horizontalwizard');
-
-new Wizard('#verticalwizard');
